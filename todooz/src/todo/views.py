9335406d15a235c614a8c5927456b0853b721c05
仁愛 朴 <inae1park@jinai-no-MacBook-Pro.local>
2021-03-01 10:13:57 -0800

    added more

--- todooz/src/todo/views.py
+++ todooz/src/todo/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError 
 from django.contrib.auth import login, logout, authenticate
 from .forms import TodoForm
 from .models import Todo
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
@@ -34,12 +36,13 @@ def signupuser(request):
             return render(request, 'todo/signupuser.html', {'form': UserCreationForm(),'error': 'Password did not match'})
 
 
-
+@login_required
 def logoutuser(request):
     if request.method == 'POST':
         logout(request)
         return redirect('home')
 
+@login_required
 def loginuser(request):
     if request.method == 'GET':
         return render(request, 'todo/loginuser.html', {'form': AuthenticationForm()})
@@ -65,6 +68,10 @@ def currenttodos(request):
     todos = Todo.objects.all()
     return render(request, 'todo/currenttodos.html', {'todos': todos})
 
+def completetodos(request):
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=false)
+    return render(request, 'todo/currenttodos.html', {'todos': todos})
+
 def createtodo(request):
     if request.method == 'GET':
         return render(request, 'todo/createtodo.html', {'form': TodoForm()})
@@ -84,3 +91,34 @@ def createtodo(request):
                     'error': 'Bad data!!!!!!!!!'
                 }
             )
+
+
+def viewtodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'GET':
+        form = TodoForm(request.POST)
+        return render(request, 'todo/viewtodo.html', {'todo' : todo, 'form': form})
+    else:
+        try:
+            form = TodoForm(request.POST, instance=todo)            
+            form.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/viewtodo.html', {'todo': todo, 'error': 'Bad imformation'})
+
+def deletetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('currenttodos')
+
+def completetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
+    if request.method == 'POST':
+        todo.datecompleted = timezone.now()
+        todo.save()
+        return redirect('currenttodos')
+
+
+def handler404(request, exception):
+    return renter(request, 'todo/404.html', status=404)
--- todooz/src/todooz/urls.py
+++ todooz/src/todooz/urls.py
@@ -26,5 +26,15 @@ urlpatterns = [
 
     path('', views.home, name='home'),
     path('current/', views.currenttodos, name='currenttodos'),
+    path('completed/', views.completetodos, name='completetodos'),
+
     path('create/', views.createtodo, name='createtodo'),
+    path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
+    path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
+    path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
+
+
+
 ]
+
+handler404 = views.handler404
